de09fa758e526ba3ba2ead206b6d0080bbfe8f83
poojithkumar29 <poojithkumar.p123@gmail.com>
2026-07-15 16:33:17 +0530

    k8

---
+++ 90-eks/variabels.tf
@@ -0,0 +1,35 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "daws1.shop"
+}
+
+variable "zone_id" {
+    default = "Z03050851O7EFPQ87AMM1"
+}
+
+variable "eks_version" {
+
+}
+
+variable "eks_nodegroup_blue_version" {
+
+}
+
+variable "eks_nodegroup_green_version" {
+
+}
+
+variable enable_blue {
+
+}
+
+variable enable_green {
+    
+}
