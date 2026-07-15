a3bedd8bc0fc2acc1d21f83e04ac456fee0c3db7
poojithkumar29 <poojithkumar.p123@gmail.com>
2026-07-15 16:17:06 +0530

    k8

---
+++ 80-frontend-alb/variabels.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "daws1.shop"
+}
+
+variable "zone_id" {
+    default = "Z03050851O7EFPQ87AMM1"
+}
